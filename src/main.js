d3a41cb6c0cc0056bd737d66f49dbf95936480c0
binqbit <128422584+binqbit@users.noreply.github.com>
2024-07-14 03:51:41 +0300

    Add start, stop, and launch new configuration commands

--- src/main.js
+++ src/main.js
@@ -2,16 +2,30 @@ const fs = require("fs");
 const { getAllExternalPorts, normalizeNginxConfig } = require("./nginx_parser");
 const { stopNginxContainer, startNginxContainer, setNginxConfig } = require("./config");
 
-const NGINX_PATH = process.argv[2];
+const CMD = process.argv[2];
 
-if (!NGINX_PATH) {
-    console.error("Please provide path to nginx config");
-    process.exit(1);
+switch (CMD) {
+    case "start":
+        startNginxContainer(() => {
+            process.exit(0);
+        });
+        break;
+    case "stop":
+        stopNginxContainer(() => {
+            process.exit(0);
+        });
+        break;
+    default:
+        const NGINX_PATH = process.argv[2];
+
+        if (!NGINX_PATH) {
+            console.error("Please provide path to nginx config");
+            process.exit(1);
+        }
+
+        const nginx = normalizeNginxConfig(fs.readFileSync(NGINX_PATH, "utf8"));
+        const ports = getAllExternalPorts(nginx);
+
+        setNginxConfig(nginx, ports);
+        stopNginxContainer(startNginxContainer);
 }
-
-const nginx = normalizeNginxConfig(fs.readFileSync(NGINX_PATH, "utf8"));
-const ports = getAllExternalPorts(nginx);
-
-setNginxConfig(nginx, ports);
-stopNginxContainer();
-startNginxContainer();
